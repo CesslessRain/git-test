5a53af2d45e7299134d6def8f29dfc6d5568bac6
CesslessRain <111259522+CesslessRain@users.noreply.github.com>
2022-08-15 14:51:31 +0800

    Update GitTest.java

--- src/main/java/com/woniu/git/GitTest.java
+++ src/main/java/com/woniu/git/GitTest.java
@@ -13,6 +13,6 @@ public class GitTest {
         System.out.println("4th");
         System.out.println("master test!");
         System.out.println("hot-fix test");
-
+        System.out.println("pull test");
     }
 }

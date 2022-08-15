8939096d83e1199f402f334a8bc1f61768c7323e
Rain <1628973617@qq.com>
2022-08-15 12:05:50 +0800

    2th commit

--- src/main/java/com/woniu/git/GitTest.java
+++ src/main/java/com/woniu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("2th");
+        System.out.println("3th");
     }
 }

5fed762b529ad66c5e3c4c32393a33519779dd71
Rain <1628973617@qq.com>
2022-08-15 12:03:03 +0800

    first commit

---
+++ src/main/java/com/woniu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.woniu.git;
+
+/**
+ * @author YoungRain
+ * @date 2022/08/15 11:58
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
